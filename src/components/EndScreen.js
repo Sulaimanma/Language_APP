9dd59e2cac6b46a1c5851ccdcc37372cb5dd2bbc
Sulaimanma <379043678@qq.com>
2021-03-11 16:01:31 +1000

    set up the switch question button and score system

--- src/components/EndScreen.js
+++ src/components/EndScreen.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React,{useContext} from 'react'
+import { QuizContext } from '../Helpers/Contexts'
+import {Questions} from '../Helpers/QuestionBank'
+import "../App.css"
+
 
 export default function EndScreen() {
+     const {score}=useContext(QuizContext)
     return (
-        <div>
+        <div className="endScreen">
+            <h1>Quiz finshed</h1>
             
         </div>
     )
