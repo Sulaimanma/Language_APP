d3ece28b4c674c8dbedfce00c9fc2d424a55dcf6
Sulaimanma <379043678@qq.com>
2021-03-25 16:34:31 +1000

    Add toggle icons for learn and quiz

--- src/components/LessonCard/LessonCard.jsx
+++ src/components/LessonCard/LessonCard.jsx
@@ -1,38 +1,63 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import classes from "./lessoncard.module.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Row, Col, Image, ProgressBar } from "react-bootstrap";
 import { QuizContext } from "../../Helpers/Contexts";
-
+import { ImBooks } from "react-icons/im";
+import { BsFillQuestionCircleFill } from "react-icons/bs";
+import option from "../../img/option.png";
 export default function LessonCard(props) {
   const { setGameState, setLanguage, language } = useContext(QuizContext);
+  const [expand, setExpand] = useState(false);
   return (
-    <Row className={classes.CardDiv} onClick={() => setGameState("quiz")}>
-      <Col md={2}>
-        <Image
-          className={classes.ImageDiv}
-          roundedCircle
-          src={props.imgUrl}
-        ></Image>
-      </Col>
-      <Col md={6}>
-        <div className={classes.TextDiv}>
-          <Row>
-            {" "}
-            <h2> {props.title}</h2>
-          </Row>
-          <Row>
-            <p> {props.intro}</p>
-          </Row>
-        </div>
-      </Col>
-      <Col md={4}>
-        <ProgressBar
-          className={classes.ProgressBar}
-          variant="success"
-          now={40}
-        />
-      </Col>
-    </Row>
+    <>
+      <Row
+        className={classes.CardDiv}
+        onClick={() => {
+          //   setGameState("quiz");
+          setExpand(!expand);
+        }}
+      >
+        <Col md={2}>
+          <Image
+            className={classes.ImageDiv}
+            roundedCircle
+            src={props.imgUrl}
+          ></Image>
+        </Col>
+        <Col md={6}>
+          <div className={classes.TextDiv}>
+            <Row>
+              {" "}
+              <h2> {props.title}</h2>
+            </Row>
+            <Row>
+              <p> {props.intro}</p>
+            </Row>
+          </div>
+        </Col>
+        <Col md={4}>
+          <ProgressBar
+            className={classes.ProgressBar}
+            variant="success"
+            now={40}
+          />
+        </Col>
+      </Row>
+      {expand && (
+        <Row className={classes.optionDiv}>
+          <Col xs={{ span: 4, offset: 1 }}>
+            <Image src={option} className={classes.optionImg}></Image>
+            <ImBooks className={classes.learn} />
+            <BsFillQuestionCircleFill
+              className={classes.quiz}
+              onClick={() => {
+                setGameState("quiz");
+              }}
+            />
+          </Col>
+        </Row>
+      )}
+    </>
   );
 }
